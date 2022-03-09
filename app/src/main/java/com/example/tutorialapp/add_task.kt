95e580a238cd0e82a49b937cb6489c1ecd01a13c
marcverdoorn <marc.verdoorn@outlook.com>
2022-03-09 16:43:17 +0100

    added code to edit layout programmatically

---
+++ app/src/main/java/com/example/tutorialapp/add_task.kt
@@ -0,0 +1,11 @@
+package com.example.tutorialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_task : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
--- app/src/main/java/com/example/tutorialapp/tasks.kt
+++ app/src/main/java/com/example/tutorialapp/tasks.kt
@@ -1,18 +1,47 @@
 package com.example.tutorialapp
 
+import android.app.ActionBar
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
+import android.util.TypedValue
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_tasks.*
 
 class tasks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
+        for (i in (0..30)){
+            add_task_view(i, "throw out the garbage")
+        }
+    }
 
-        val taskview = TextView(this)
+    fun add_task(view: View){
+        val intent = Intent(this, add_task::class.java).apply {  }
+        startActivity(intent)
+    }
+
+    fun add_task_view(color: Int, text: String){
+        val taskview: TextView = TextView(this)
         taskview.textSize = 20f
-        taskview.text = "Do a task"
+        taskview.text = text
+        taskview.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        taskview.setPadding(10,50,10,50)
+
+        taskview.setBackgroundColor(Color.rgb(230-color*5, 19+color*3, color*6))
         task_layout.addView(taskview)
+
+        val done: Button = Button(this)
+        done.text = "DONE"
+        done.width = 120
+        done.height = 60
+
+        task_layout.addView(done)
     }
 }
